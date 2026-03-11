fd9cd210387fc985c96b2695bc8d8fe0c5eb52c7
Denison Leandro S Carneiro <denisonleandro7@gmail.com>
2026-03-11 01:48:29 -0300

    Aula02

---
+++ Aula02/Aula02.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite seu nome:");
+            String nome = scanner.nextLine();
+
+            System.out.println("Olá " + nome);
+        }
+    }
+	
+}
